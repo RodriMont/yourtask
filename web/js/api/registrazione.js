31d49e92fcbd3a9620e1155459272671180cb072
Rodrigo Sebastian Montero Mozombite <rodrigo.montero@itsrizzoli.it>
2024-03-18 17:52:44 +0100

    web registrazione

---
+++ web/js/api/registrazione.js
@@ -0,0 +1,60 @@
+import axios from "https://cdn.jsdelivr.net/npm/axios@1.3.5/+esm";
+
+export async function registrazione() {
+  const email = document.querySelector(".email");
+  const password = document.querySelector(".password");
+  const username = document.querySelector(".username");
+
+  if ([email, password, username].includes("")) {
+    alert("Tutti campi sono obbligatori");
+    return false;
+  }
+
+  const data = {
+    email: email.value,
+    username: username.value,
+    password: password.value,
+  };
+  try {
+
+
+    /* const request = await fetch("http://127.0.0.1:5000/registrazione", {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
+      body: JSON.stringify(data),
+    }); */
+
+    const request = await axios.post(
+      "http://127.0.0.1:5000/registrazione",
+      data
+    );
+
+    if (!request.data.ok) {
+      alert(`Email: ${email} gia in uso`);
+      return false;
+    }
+
+    const loginBtn = document.querySelector("#login");
+
+    loginBtn.click();
+
+    alert("Usuario creato");
+
+    email.value = ""
+    username.value = ""
+    password.value = ""
+    
+
+    return true;
+  } catch (err) {
+    console.log("error: " + err);
+  }
+}
--- web/js/script.js
+++ web/js/script.js
@@ -1,3 +1,16 @@
+import { registrazione } from "./api/registrazione.js";
 import { loginUI } from "./ui/login.js";
 
-loginUI()
+const registrazioneBtn = document.querySelector("#btn-registrazione");
+
+document.addEventListener("DOMContentLoaded", () => {
+  loginUI();
+});
+
+registrazioneBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+
+  await registrazione();
+    
+  
+});
