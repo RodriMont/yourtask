b62fadd671539ee5bd4b96b1d2fccdc221d35ad1
Rodrigo Sebastian Montero Mozombite <rodrigo.montero@itsrizzoli.it>
2024-03-18 15:17:52 +0100

    sever

---
+++ server/app.py
@@ -0,0 +1,78 @@
+from flask import Flask, request
+import pymysql
+import json
+from model import Utente
+
+db = pymysql.connect(host="127.0.0.1", port=3306, user="root", password="1234", database="yourtask", autocommit=True)
+app = Flask(__name__)
+
+cursor = db.cursor()
+
+
+@app.route("/registrazione", methods=["POST"])
+def registrazione_utente():
+    data = request.get_json();
+    #cusor = db.cursor()
+    username = data["username"]
+    email = data["email"]
+    password = data["password"]
+
+    res = {
+        "ok": False,
+    }
+
+    try:
+        user = get_user_by_email(email)
+        if(len(user) == 0):
+            query = "INSERT INTO utente(username, email, password) VALUES (%s, %s, %s )"
+            cursor.execute(query, (username, email, password))
+
+            res["ok"] = True
+            res["msg"] = "Usuario Creato "
+            db.commit()
+        else:
+            res["ok"] = False
+            res["msg"] = "Usuario non creato, email usato"
+
+    except Exception as e:
+        print(e)
+        res["ok"] = False
+        res["msg"] = f"Errore server: {e}"
+
+    return json.dumps(res)
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    email = data["email"]
+    password = data["password"]
+
+    res = {
+        "ok": False
+    }
+
+    try:
+        utente_db = get_user_by_email(email)
+        if(len(utente_db) == 0):
+            res["ok"] = False
+        else:
+            utente_db = utente_db[0]
+            if(utente_db[3] == password):
+                res["ok"] = True
+
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+    return res
+
+def get_user_by_email(email:str):
+    query = "SELECT * FROM utente WHERE email= %s"
+    cursor.execute(query, (email))
+    user = cursor.fetchall();
+    return user
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ server/model.py
@@ -0,0 +1,72 @@
+class Progetto:
+    id = None
+    nome_progetto = None
+    data_avvio = None
+    data_scadenza = None
+    budget = None
+
+    def __init__(self, id, nome_progetto, data_avvio, data_scadenza, budget):
+        self.id = id
+        self.nome_progetto = nome_progetto
+        self.data_avvio = data_avvio
+        self.data_scadenza = data_scadenza
+        self.budget = budget
+
+
+class Utente:
+    id = None
+    username = None
+    email = None
+    password = None
+
+    def __init__(self, id, username, email, password):
+        self.id = id
+        self.username = username
+        self.email = email
+        self.password = password
+
+class Ruolo:
+    id = None
+    nome_ruolo = None
+    id_task = None
+
+    def __init__(self, id, nome_ruolo, id_task):
+        self.id = id
+        self.nome_ruolo = nome_ruolo
+        self.id_task = id_task
+
+class Task:
+    id = None
+    nome_task = None
+    data_avvio = None
+    data_scadenza = None
+    priorita = None
+
+    def __init__(self, id, nome_task, data_avvio, data_scadenza, priorita):
+        self.id = id
+        self.nome_task = nome_task
+        self.data_avvio = data_avvio
+        self.data_scadenza = data_scadenza
+        self.priorita = priorita
+
+class RuoloUtente:
+    id_ruolo = None
+    id_utente = None
+    id_task = None
+
+    def __init__(self, id_ruolo, id_utente, id_task):
+        self.id_ruolo = id_ruolo
+        self.id_utente = id_utente
+        self.id_task = id_task
+
+class Lavori:
+    id_utente = None
+    id_progetto = None
+    id_task = None
+    id_ruolo = None
+
+    def __init__(self, id_utente, id_progetto, id_task, id_ruolo):
+        self.id_utente = id_utente
+        self.id_progetto = id_progetto
+        self.id_task = id_task
+        self.id_ruolo
