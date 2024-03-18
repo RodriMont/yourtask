931e289692ae0f1e9dc835028c2b969e66b7e826
GabrieleRZ <gabriele.dalessandro@itsrizzoli.it>
2024-03-18 16:26:34 +0100

    File flask con richieste GET

--- server/app.py
+++ server/app.py
@@ -1,75 +1,97 @@
 from flask import Flask, request
 import pymysql
 import json
+from model import *
 
-db = pymysql.connect(host="127.0.0.1", port=3306, user="root", password="1234", database="yourtask", autocommit=True)
 app = Flask(__name__)
 
-cursor = db.cursor()
+connection = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="yourtask",
+    port=3306
+)
 
+cursor = connection.cursor()
 
-@app.route("/registrazione", methods=["POST"])
-def registrazione_utente():
-    data = request.get_json()
-    #cusor = db.cursor()
-    username = data["username"]
-    email = data["email"]
-    password = data["password"]
+@app.route('/')
+def index():
+    return "Hello World!"
 
-    res = {
-        "ok": False,
-    }
+@app.route("/progetti_utente")
+def progetti_utente():
+    id_utente = request.args.get('id')
 
-    try:
-        user = get_user_by_email(email)
-        if(len(user) == 0):
-            query = "INSERT INTO utente(username, email, password) VALUES (%s, %s, %s )"
-            cursor.execute(query, (username, email, password))
+    cursor.execute(f"""select progetto.id, progetto.nome_progetto, progetto.data_avvio, progetto.data_scadenza, progetto.budget
+                    from utenteprogetto
+                    inner join progetto
+                    on utenteprogetto.id_progetto = progetto.id
+                    where utenteprogetto.id_utente = {id_utente}""")
+    
+    rows = cursor.fetchall()
+    progetti = []
 
-            res["ok"] = True
-            res["msg"] = "Usuario Creato "
-            db.commit()
-        else:
-            res["ok"] = False
-            res["msg"] = "Usuario non creato, email usato"
+    for row in rows:
+        id = row[0]
+        nome_progetto = row[1]
+        data_avvio = row[2]
+        data_scadenza = row[3]
+        budget = row[4]
 
-    except Exception as e:
-        print(e)
-        res["ok"] = False
-        res["msg"] = f"Errore server: {e}"
+        progetto = Progetto(id, nome_progetto, str(data_avvio), str(data_scadenza), budget)
+        progetti.append(progetto.__dict__)
 
-    return json.dumps(res)
+    return json.dumps(progetti)
 
-@app.route("/login", methods=["POST"])
-def login():
-    data = request.get_json()
-    email = data["email"]
-    password = data["password"]
+@app.route("/utente_email")
+def utente_email():
+    email = request.args.get('email')
 
-    res = {
-        "ok": False
-    }
+    cursor.execute(f"""select id, username, email, password
+                       from utente
+                       where email = \"{email}\"""")
+    
+    rows = cursor.fetchall()
+    utenti = []
 
-    try:
-        utente_db = get_user_by_email(email)
-        if(len(utente_db) == 0):
-            res["ok"] = False
-        else:
-            utente_db = utente_db[0]
-            if(utente_db[3] == password):
-                res["ok"] = True
+    for row in rows:
+        id = row[0]
+        username = row[1]
+        email = row[2]
+        password = row[3]
 
+        utente = Utente(id, username, email, password)
+        utenti.append(utente.__dict__)
 
-    except Exception as e:
-        print(f"Error: {e}")
+    return json.dumps(utenti)
 
-    return res
+@app.route("/task_utente")
+def task_utente():
+    id_utente = request.args.get('id_utente')
+    id_progetto = request.args.get('id_progetto')
 
-def get_user_by_email(email:str):
-    query = "SELECT * FROM utente WHERE email= %s"
-    cursor.execute(query, (email))
-    user = cursor.fetchall()
-    return user
+    cursor.execute(f"""select task.id, task.nome_task, task.data_avvio, task.data_scadenza, task.priorita, task.id_progetto
+                       from taskutente
+                        inner join task
+                        on task.id = taskutente.id_task
+                        where id_utente = {id_utente} and task.id_progetto = {id_progetto}""")
+
+    rows = cursor.fetchall()
+    tasks = []
+
+    for row in rows:
+        id = row[0]
+        nome_task = row[1]
+        data_avvio = row[2]
+        data_scadenza = row[3]
+        priorita = row[4]
+        id_progetto = row[5]
+
+        task = Task(id, nome_task, str(data_avvio), str(data_scadenza), priorita, id_progetto)
+        tasks.append(task.__dict__)
+
+    return json.dumps(tasks)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
