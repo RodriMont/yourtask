f5ba80df640e80fe724bbff869b578afaa8027c8
GabrieleRZ <gabriele.dalessandro@itsrizzoli.it>
2024-03-20 17:20:44 +0100

    Iniziata pagina di registrazione

--- app/app/src/main/java/com/example/yourtask/MainActivity.java
+++ app/app/src/main/java/com/example/yourtask/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
     }
 }
