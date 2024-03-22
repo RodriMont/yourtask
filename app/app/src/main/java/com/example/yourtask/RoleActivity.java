227ea91fea001a98a2a4a2668105c27140155057
GesGabbo <gabriele.piazzolla@itsrizzoli.it>
2024-03-22 12:53:50 +0100

    Rolecreate

---
+++ app/app/src/main/java/com/example/yourtask/RoleActivity.java
@@ -0,0 +1,13 @@
+package com.example.yourtask;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RoleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_role);
+    }
+}
