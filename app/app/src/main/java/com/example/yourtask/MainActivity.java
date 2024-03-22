cbca02ef64e48a00ec3b1251a1b939d488fcbf89
GabrieleRZ <gabriele.dalessandro@itsrizzoli.it>
2024-03-22 12:21:45 +0100

    Aggiunta barra di navigazione

--- app/app/src/main/java/com/example/yourtask/MainActivity.java
+++ app/app/src/main/java/com/example/yourtask/MainActivity.java
@@ -1,16 +1,55 @@
 package com.example.yourtask;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity
 {
+    private BottomNavigationView bottomNavigationMenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        bottomNavigationMenu = (BottomNavigationView)findViewById(R.id.bottom_navigation_bar);
+        bottomNavigationMenu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item)
+            {
+                int id = item.getItemId();
+                item.setChecked(true);
+
+                if (id == R.id.navigation_back)
+                    changeFragment(new ProfileFragment());
+                else if (id == R.id.navigation_home)
+                    changeFragment(new ProfileFragment());
+                else if (id == R.id.navigation_profile)
+                    changeFragment(new ProfileFragment());
+
+                return false;
+            }
+        });
+    }
+
+    private boolean changeFragment(Fragment fragment)
+    {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+        return true;
     }
 }
