196d75093b253da43e84940ac049d1b27ee9ad5c
RodrigoSebastianMont <rodrigo.montero@itsrizzoli.it>
2024-03-22 12:34:41 +0100

    homepage fatto

--- app/app/src/main/java/com/example/yourtask/HomepageActivity.java
+++ app/app/src/main/java/com/example/yourtask/HomepageActivity.java
@@ -1,17 +1,16 @@
 package com.example.yourtask;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity
-{
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomepageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.homepage_layout);
     }
+
+
 }
