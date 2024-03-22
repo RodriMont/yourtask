69cda8421f89f5bc47cf13d255d20e9796169f39
RodrigoSebastianMont <rodrigo.montero@itsrizzoli.it>
2024-03-22 16:22:17 +0100

    create project fragment

--- app/app/src/main/java/com/example/yourtask/MainActivity.java
+++ app/app/src/main/java/com/example/yourtask/MainActivity.java
@@ -1,56 +1,62 @@
-package com.example.yourtask;
+    package com.example.yourtask;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
+    import androidx.annotation.NonNull;
+    import androidx.appcompat.app.AppCompatActivity;
+    import androidx.core.content.ContextCompat;
+    import androidx.fragment.app.Fragment;
 
-import android.content.Intent;
-import android.content.res.ColorStateList;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.os.Bundle;
-import android.view.MenuItem;
+    import android.content.Intent;
+    import android.content.res.ColorStateList;
+    import android.graphics.Color;
+    import android.graphics.PorterDuff;
+    import android.os.Bundle;
+    import android.util.Log;
+    import android.view.MenuItem;
+    import android.widget.Button;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
+    import com.google.android.material.bottomnavigation.BottomNavigationView;
+    import com.google.android.material.navigation.NavigationBarView;
 
-public class MainActivity extends AppCompatActivity
-{
-    private BottomNavigationView bottomNavigationMenu;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
+    public class MainActivity extends AppCompatActivity
     {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        private BottomNavigationView bottomNavigationMenu;
 
-        getSupportActionBar().hide();
-
-        bottomNavigationMenu = (BottomNavigationView)findViewById(R.id.bottom_navigation_bar);
-        bottomNavigationMenu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener()
+        @Override
+        protected void onCreate(Bundle savedInstanceState)
         {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem item)
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            getSupportActionBar().hide();
+
+            bottomNavigationMenu = (BottomNavigationView)findViewById(R.id.bottom_navigation_bar);
+            bottomNavigationMenu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener()
             {
-                int id = item.getItemId();
-                item.setChecked(true);
+                @Override
+                public boolean onNavigationItemSelected(MenuItem item)
+                {
+                    int id = item.getItemId();
+                    item.setChecked(true);
 
-                if (id == R.id.navigation_back)
-                    changeFragment(new ProfileFragment());
-                else if (id == R.id.navigation_home)
-                    changeFragment(new ProfileFragment());
-                else if (id == R.id.navigation_profile)
-                    changeFragment(new ProfileFragment());
+                    if (id == R.id.navigation_back)
+                        changeFragment(new ProfileFragment());
+                    else if (id == R.id.navigation_home)
+                        changeFragment(new HomepageFragment());
+                    else if (id == R.id.navigation_profile)
+                        changeFragment(new ProfileFragment());
 
-                return false;
-            }
-        });
+                    return false;
+                }
+            });
+
+
+        }
+
+        public boolean changeFragment(Fragment fragment)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+            return true;
+        }
+
+
     }
-
-    private boolean changeFragment(Fragment fragment)
-    {
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
-        return true;
-    }
-}
